f2a5f38b290ac198c3c4d035a52e6784283e61a3
boris <borislav2003@gmail.com>
2025-02-26 23:53:10 +0500

    feat: step directive feat: siteguide service

--- projects/ngx-siteguide/src/lib/siteguide-step.directive.ts
+++ projects/ngx-siteguide/src/lib/siteguide-step.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, inject, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, inject, Input, OnInit, TemplateRef, Type } from '@angular/core';
 import { TourStepConfig } from 'siteguide.js';
 import { SiteguideService } from './siteguide.service';
 
@@ -10,9 +10,23 @@ export class SiteguideStepDirective implements OnInit {
     @Input({ required: true, alias: 'siteguideStep' })
     public readonly stepData!: TourStepConfig;
 
+    @Input({ required: false })
+    public readonly template: TemplateRef<any> | null = null;
+
+    @Input({ required: false })
+    public readonly component: Type<any> | null = null;
+
+    private readonly _elementRef: ElementRef<any> = inject(ElementRef);
     private readonly _siteguideService: SiteguideService = inject(SiteguideService);
 
     public ngOnInit(): void {
-        this._siteguideService.addStep(this.stepData);
+        this._siteguideService.addStep(
+            {
+                ...this.stepData,
+                host: this._elementRef.nativeElement,
+            },
+            this.component,
+            this.template
+        );
     }
 }
--- projects/ngx-siteguide/src/lib/siteguide.service.ts
+++ projects/ngx-siteguide/src/lib/siteguide.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+import { createComponent, EnvironmentInjector, inject, Injectable, TemplateRef, Type } from '@angular/core';
 import { Tour, TourConfig, TourStepConfig } from 'siteguide.js';
 
 @Injectable()
 export class SiteguideService {
     public readonly tour: Tour;
+
+    private readonly _envInjector: EnvironmentInjector = inject(EnvironmentInjector);
 
     constructor() {
         this.tour = new Tour({});
@@ -27,8 +29,13 @@ export class SiteguideService {
      * Add step to the tour
      * @param stepConfig step config data
      */
-    public addStep(stepConfig: TourStepConfig): void {
-        this.tour.addStep(stepConfig);
+    public addStep(
+        stepConfig: TourStepConfig,
+        component: Type<any> | null = null,
+        template: TemplateRef<any> | null = null
+    ): void {
+        const config: TourStepConfig = this.updateStepConfig(stepConfig, component, template);
+        this.tour.addStep(config);
     }
 
     /**
@@ -40,10 +47,61 @@ export class SiteguideService {
     }
 
     /**
+     * Add step right after active one
+     * @param stepConfig step config data
+     */
+    public addNext(
+        stepConfig: TourStepConfig,
+        component: Type<any> | null = null,
+        template: TemplateRef<any> | null = null
+    ): void {
+        const config: TourStepConfig = this.updateStepConfig(stepConfig, component, template);
+        this.tour.addNext(config);
+    }
+
+    /**
      * Set tour config
      * @param config tour options
      */
     public setTourConfig(config: TourConfig): void {
         this.tour.setConfig(config);
+    }
+
+    private getComponentNativeElement(component: Type<any>): HTMLElement {
+        const componentRef = createComponent(component, {
+            environmentInjector: this._envInjector,
+        });
+
+        const nativeElement: HTMLElement = componentRef.location.nativeElement as HTMLElement;
+
+        return nativeElement;
+    }
+
+    private updateStepConfig(
+        stepConfig: TourStepConfig,
+        component: Type<any> | null,
+        template: TemplateRef<any> | null
+    ): TourStepConfig {
+        if (component) {
+            stepConfig = {
+                ...stepConfig,
+                popup: {
+                    ...stepConfig.popup,
+                    type: 'custom',
+                    node: this.getComponentNativeElement(component),
+                },
+            };
+        } else if (template) {
+            stepConfig = {
+                ...stepConfig,
+                popup: {
+                    ...stepConfig.popup,
+                    type: 'custom',
+                    node: template.elementRef.nativeElement,
+                },
+            };
+        }
+
+        return stepConfig;
     }
 }
